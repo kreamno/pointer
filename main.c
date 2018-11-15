dd979af52e3897c95860043a75d9e8c439ce0b13
yujeong <kreamno@gmail.com>
2018-11-15 16:07:08 +0900

    pointer4

--- main.c
+++ main.c
@@ -1,18 +1,48 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-char *proverb="All that glisters is not gold.";
 
-void setPointer(char **q)	
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int addfunc(int a, int b)
 {
-	*q = proverb;	
+	return a+b;
 }
-
-int main(void)
+int subfunc(int a, int b)
 {
-	char *p = "zzz";
-	setPointer(&p);
-	printf("%s\n", *p);
+	return a-b;
+}
+int mulfunc(int a, int b)
+{
+	return a*b;
+}
+int divfunc(int a, int b)
+{
+	return a/b;
+}
+int main(int argc, char *argv[]) {
+	
+	int num1, num2;
+	char op;
+	int(*calcfunc)(int, int);
+	
+	printf("input calculation : ");
+	scanf("%d %c %d", &num1, &op, &num2);
+	switch(op)
+	{			
+		case '+':
+			calcfunc = addfunc;
+			break;
+		case '-':
+			calcfunc = subfunc;
+			break;
+		case '*':
+			calcfunc = mulfunc;
+			break;
+		case '/':
+			calcfunc = divfunc;	
+			break;
+	}
+	
+	printf("result : %i\n", calcfunc(num1,num2));
 	
 	return 0;
 }
